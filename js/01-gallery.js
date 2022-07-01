46349443845c769e14918136824ad88f025cffc8
Oleksii Chyharov <chyharov@gmail.com>
2022-07-01 18:35:31 +0300

    01-gallery ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryWraper = document.querySelector(".gallery");
+let makeGalery;
+
+function createBlockWithImage({ preview, original, description }) {
+  return ` <div class="gallery__item">
+        <a class="gallery__link" href = "${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+}
+
+galleryWraper.insertAdjacentHTML(
+  "beforeend",
+  galleryItems.map(createBlockWithImage).join("")
+);
+
+galleryWraper.addEventListener("click", galleryClick);
+
+function galleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const urlEl = event.target.dataset.source;
+
+  makeGalery = basicLightbox.create(`
+      <img src="${urlEl}" width="800" height="600">
+  `);
+
+  makeGalery.show();
+
+  document.addEventListener("keydown", onKeyboardPress);
+}
+
+function onKeyboardPress(event) {
+  if (event.code === "Escape") {
+    makeGalery.close();
+  }
+  document.removeEventListener("keydown", onKeyboardPress);
+}
